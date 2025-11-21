e2348e2bcbcc0bee731ce728770de577092f0014
Akibaten <sarahsanders0814@gmail.com>
2025-11-21 17:28:10 -0600

    save point. I need to make parsing logic for keyword indexer scrapy spider

--- scripts/keyword_indexer.py
+++ scripts/keyword_indexer.py
@@ -7,6 +7,10 @@ import re
 import sqlite3
 import string
 from tqdm import tqdm
+import numpy as np
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
 indexedwords = []
 #not indexed words currently not implemented
 no_index_words = [
@@ -30,7 +34,37 @@ id_db_cursor.execute(
              id,
              unique(word,id))
              """)
+
 id_site_list = stats_db_cursor.execute("""SELECT id, site_url FROM website""").fetchall()
+
+
+print(id_site_list)
+site_lists = [site_list.tolist() for site_list in np.array_split(id_site_list,4)]
+print(site_lists[0])
+
+print([len(list) for list in site_lists])
+
+class KeywordSpider(scrapy.Spider):
+    name = "keywordspider"
+
+    def __init__(self, site_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.urls = [str(url[1]) for url in site_list]
+
+    async def start(self):
+        for url in self.urls[:5]:
+            yield scrapy.Request(url=url, callback=self.parse)
+     
+    def parse(self, response):
+        #CREATE PARSING LOGIC HERE NOT DONE YET xd
+
+
+
+
+
+
+
+
 def add_keyword(url, document_id):
     site_html = requests.get(url).content.decode("utf-8")
     site_parser = BeautifulSoup(site_html, 'html.parser')
@@ -41,12 +75,26 @@ def add_keyword(url, document_id):
             """INSERT OR IGNORE INTO keyword_list(word,id) VALUES
                     (?, ?)""", (word,document_id)
         )
-#progress bar for my sanity lol
-for site in tqdm(id_site_list):
-    try:
-        add_keyword(url=site[1], document_id=site[0])
-        id_db.commit()
-    except Exception as e:
-        continue
+# #progress bar for my sanity lol
+# for site in tqdm(id_site_list):
+#     try:
+#         add_keyword(url=site[1], document_id=site[0])
+#         id_db.commit()
+#     except Exception as e:
+#         continue
+
+crawler = CrawlerProcess(settings={
+    'DOWNLOAD_DELAY': 0.25,
+    'LOG_LEVEL':  'DEBUG',
+    
+    # GRRRRR This is important
+    # with multiple concurrent requests
+    # sometimes in edge cases duplicates
+    # will accidentally bypass the filter
+    'CONCURRENT_REQUESTS': 10,
+})
+
+crawler.crawl(KeywordSpider,site_list=site_lists[0])
+crawler.start()
 
 id_db_cursor.execute("VACUUM")
